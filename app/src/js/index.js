de747e97bbdc7e5eeb2f4e3240c0610e67157f8a
julite79 <61328231+julite79@users.noreply.github.com>
2020-07-31 04:35:18 +0200

    ..

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -32,21 +32,24 @@ $(document).ready(function () {
         window.automated_carousels.push(this);
     }
 
+    //Get the button:
+    mybutton = document.getElementById("myBtn");
+
 // When the user scrolls down 20px from the top of the document, show the button
     window.onscroll = function() {scrollFunction()};
 
     function scrollFunction() {
-        let mybutton = document.getElementById("myBtn");
         if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-            mybutton.classList.add('visible');
+            mybutton.style.display = "block";
         } else {
-            mybutton.classList.remove('visible');
+            mybutton.style.display = "none";
         }
     }
 
 // When the user clicks on the button, scroll to the top of the document
     function topFunction() {
-        $("html, body").animate({ scrollTop: 0 }, 600);
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
     }
 
     (function () {
